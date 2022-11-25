7929e9279fba235ba6dd3a8d8c938ddb3784dea8
dyh_a <xinzon32177@163.com>
2022-11-26 03:11:43 +0800

    chore: update useFile

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -37,12 +37,13 @@ export function useFile<
   P extends boolean = false,
   C = P extends true ? Record<string, any> : string,
 >(path: string, parseJson = false as P): [C, (content: C) => void] {
-  let content: string | Record<string, any> = fs.readFileSync(path).toString();
-  if (parseJson) content = JSON.parse(content);
+  let _content: string | Record<string, any> | null = fs.readFileSync(path).toString();
+  if (parseJson) _content = JSON.parse(_content);
 
   return [
-    content as C,
+    _content as C,
     (content) => {
+      _content = null;
       let result = content as string;
       if (parseJson) result = JSON.stringify(content, null, 2);
       fs.writeFileSync(path, result);
